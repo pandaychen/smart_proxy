ff4ca248d51522607ffe3c21f1dc3eaf4757068f
pandaychen <pandaychen@tencent.com>
2021-09-23 20:07:10 +0800

    增加etcd的WithPrevKV选项，删除时获取被删除的value

--- discovery/etcd/etcd.go
+++ discovery/etcd/etcd.go
@@ -2,6 +2,9 @@ package etcd
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"strings"
 
 	"smart_proxy/backend"
 	"smart_proxy/enums"
@@ -57,6 +60,14 @@ func NewEtcdDiscoveryClient(etcd_conf *EtcdConfig) (*EtcdDiscoveryClient, error)
 	return client, nil
 }
 
+func getProxyname(path string) (string, error) {
+	array := strings.Split(path, "/")
+	if len(array) < 3 {
+		return "", errors.New("illegal etcd path")
+	}
+	return array[2], nil
+}
+
 func (e *EtcdDiscoveryClient) Run() {
 	datalist, err := e.Client.GetKeyPrefixValues(e.Client.Context, e.WatchPrefix)
 	if err != nil {
@@ -65,12 +76,23 @@ func (e *EtcdDiscoveryClient) Run() {
 	}
 
 	for key, value := range datalist {
+		var metadata backend.Metadata
+		err := json.Unmarshal([]byte(value), &metadata)
+		if err != nil {
+			e.Logger.Error("EtcdDiscoveryClient json Unmarshal error", zap.String("errmsg", err.Error()))
+			continue
+		}
+		proxyname, err := getProxyname(key)
+		if err != nil {
+			continue
+		}
 		backendnode := backend.BackendNodeOperator{
 			Target: backend.BackendNode{
+				ProxyName: proxyname,
 				//State:    *atomic.NewBool(true),
 				State:    true,
-				Addr:     value,
-				Metadata: key},
+				Addr:     metadata.Addr,
+				Metainfo: metadata},
 			Op: enums.BACKEND_ADD,
 		}
 		//send to scheduler's channel
@@ -81,25 +103,42 @@ func (e *EtcdDiscoveryClient) Run() {
 	defer e.Client.Close()
 	defer e.Client.Cancel()
 	for {
-		rch := e.Client.Watch(context.Background(), e.WatchPrefix, clientv3.WithPrefix())
+		rch := e.Client.Watch(context.Background(), e.WatchPrefix, clientv3.WithPrefix(), clientv3.WithPrevKV())
 		for resp := range rch {
 			for _, ev := range resp.Events {
 				e.Logger.Info("EtcdDiscoveryClient get loadbalance events", zap.Any("events", ev))
 				var backendnode backend.BackendNodeOperator
+				var metadata backend.Metadata
+				proxyname, err := getProxyname(string(ev.Kv.Key))
+				if err != nil {
+					continue
+				}
 				if ev.Type == mvccpb.DELETE {
+					err := json.Unmarshal([]byte(ev.PrevKv.Value), &metadata)
+					if err != nil {
+						e.Logger.Error("EtcdDiscoveryClient json Unmarshal error", zap.String("errmsg", err.Error()))
+						continue
+					}
 					backendnode = backend.BackendNodeOperator{
 						Target: backend.BackendNode{
-							State:    true,
-							Addr:     string(ev.Kv.Key),
-							Metadata: string(ev.Kv.Value)},
+							State:     true,
+							ProxyName: proxyname,
+							Addr:      metadata.Addr,
+							Metainfo:  metadata},
 						Op: enums.BACKEND_DEL,
 					}
 				} else {
+					err := json.Unmarshal([]byte(ev.Kv.Value), &metadata)
+					if err != nil {
+						e.Logger.Error("EtcdDiscoveryClient json Unmarshal error", zap.String("errmsg", err.Error()))
+						continue
+					}
 					backendnode = backend.BackendNodeOperator{
 						Target: backend.BackendNode{
-							State:    true,
-							Addr:     string(ev.Kv.Key),
-							Metadata: string(ev.Kv.Value)},
+							State:     true,
+							ProxyName: proxyname,
+							Addr:      metadata.Addr,
+							Metainfo:  metadata},
 						Op: enums.BACKEND_ADD,
 					}
 				}
